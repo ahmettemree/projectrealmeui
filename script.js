a668c684ee985017790c3c3253524902d0d8985e
ahmettemree <2013ahmetemre@gmail.com>
2026-01-11 19:49:00 +0300

    Add click event listeners to app icons

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-const demo = document.getElementById("appDemo");
-const icons = document.querySelectorAll(".appIcon");
-
-function openApp(name) {
-    demo.classList.add("show");
-    icons.forEach(i => i.style.display = "none");
-    console.log(name + " açıldı");
-}
-
-function closeApp() {
-    demo.classList.remove("show");
-    setTimeout(() => icons.forEach(i => i.style.display = "flex"), 250);
-}
-
-function notifyCmd(cmd) {
-    switch(cmd) {
-        case "Bildirim":
-            alert("Bildirim geldi!");
-            break;
-        case "Geri":
-            alert("Geri tuşuna basıldı");
-            break;
-        case "Aç":
-            openApp('Demo App');
-            break;
-        case "Kapat":
-            closeApp();
-            break;
-        default:
-            console.log("Bilinmeyen komut:", cmd);
-    }
-    console.log("Komut çalıştı:", cmd);
-}
+document.querySelectorAll('.app-icon').forEach(icon => {
+    icon.addEventListener('click', () => {
+        alert(icon.innerText + " açılıyor...");
+    });
+});
